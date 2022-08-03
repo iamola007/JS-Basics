d775a4e3dcc3b9bb5849c4bb93c8f2c0037a8175
Ola Oladapo <olaoladapo007@gmail.com>
2022-08-03 23:01:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+
+// A function named max, that returns the maximum between 2 numbers.
+let number = max(2, 4);
+console.log(number);
+
+function max(a, b) {
+  return a > b ? a : b;
+}
